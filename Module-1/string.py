e06e0d7efcb467349b3187447b3b875abfb9957b
SanketTops <sanket.tops@gmail.com>
2023-05-31 17:10:14 +0530

    String

--- Module-1/string.py
+++ Module-1/string.py
@@ -1,6 +1,6 @@
-mystr="This is Python!"
+mystr="this is python!"
 
-print(mystr)
+"""print(mystr)
 print(mystr[0])
 print(mystr[-1])
 print(mystr[0:4]) #slice (range)
@@ -13,4 +13,34 @@ print(len(mystr))
 if 'This' in mystr:
     print("Yes..")
 else:
-    print("Noo")
+    print("Noo")"""
+
+# ------------------------------------------------------------- #
+#String Methods 
+
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('i','I'))
+#print(mystr.split())
+#print(mystr.split('i'))
+#print(f"My string is {mystr}")
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('this'))
+#print(mystr.startswith("This"))
+#print(mystr.endswith('!'))
+#print(mystr.index('Python'))
+#print(mystr.find('is'))
+#print(mystr.index('is'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
+
+
+
---
+++ Module-1/userinput_validation.py
@@ -0,0 +1,14 @@
+#username=input("Enter an username:")
+
+
+"""if username.isalpha():
+    print("Username is Valid!")
+else:
+    print("Invalid Username!")"""
+
+mobile=input("Enter mobile number:")
+
+if mobile.isdigit():
+    print("Valid mobile number!")
+else:
+    print("Error!")
