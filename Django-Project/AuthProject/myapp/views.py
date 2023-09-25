fdc40a601fd94cba010b0ce64832368c900b3585
SanketTops <sanket.tops@gmail.com>
2023-09-25 17:20:09 +0530

    Auth - Login and Signup

--- Django-Project/AuthProject/myapp/views.py
+++ Django-Project/AuthProject/myapp/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import signupForm
+from .models import *
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=userSignup.objects.filter(username=unm,password=pas)
+        if user: #true
+            print("Login Successfully!")
+            return redirect('home')
+        else:
+            print("Error!Login fail...")
     return render(request,'index.html')
+
+def signup(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("User created!")
+            return redirect('/')
+        else:
+            print(newuser.errors)
+    return render(request,'signup.html')
+
+def home(request):
+    return render(request,'home.html')
