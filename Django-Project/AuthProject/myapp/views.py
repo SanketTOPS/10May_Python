3a3af859e8b4b2f2507eab756934ca236a5b65a9
SanketTops <sanket.tops@gmail.com>
2023-09-27 17:32:57 +0530

    Auth with Session

--- Django-Project/AuthProject/myapp/views.py
+++ Django-Project/AuthProject/myapp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render,redirect
 from .forms import signupForm
 from .models import *
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
     if request.method=='POST':
-
         unm=request.POST['username']
         pas=request.POST['password']
 
+        fnm=userSignup.objects.get(username=unm)
+        print(f"Firstname:{fnm.firstname}")
         user=userSignup.objects.filter(username=unm,password=pas)
         if user: #true
             print("Login Successfully!")
+            #request.session['cuser']=unm #creating a session
+            request.session['cuser']=fnm.firstname
             return redirect('home')
         else:
             print("Error!Login fail...")
@@ -29,4 +33,9 @@ def signup(request):
     return render(request,'signup.html')
 
 def home(request):
-    return render(request,'home.html')
+    user=request.session.get('cuser')
+    return render(request,'home.html',{'user':user})
+
+def userlogout(request):
+    logout(request)
+    return redirect('/')
