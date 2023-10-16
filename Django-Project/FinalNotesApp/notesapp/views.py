0b7d5d19d1c62add3c9adbee56771ffe3b05c63a
SanketTops <sanket.tops@gmail.com>
2023-10-16 17:33:38 +0530

    Signup

--- Django-Project/FinalNotesApp/notesapp/views.py
+++ Django-Project/FinalNotesApp/notesapp/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from.forms import *
+from django.contrib import messages
 
 # Create your views here.
 
+status=False
 def index(request):
-    return render(request,'index.html')
+    global status
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+            status=True
+            print("Current Status:",status)
+            messages.success(request,"Signup Successfully!")
+        else:
+            print(newuser.errors)
+            status=False
+            messages.error(request,"Error!Something went wrong...Try again!")
+    return render(request,'index.html',{'status':status})
 
 def notes(request):
     return render(request,'notes.html')
